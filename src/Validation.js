f94a5e5550cdb77028cf3f9793b4d5350b492157
dlsxjzld <sinjuk1@naver.com>
2024-11-27 16:58:20 +0900

    fix(Validation.js): 실수도 받을 수 있도록 Number.isInteger에서 Number.isNaN으로 변경

--- src/Validation.js
+++ src/Validation.js
@@ -1,5 +1,4 @@
 import { escapeRegExp } from './util.js';
-import { DELIMITER } from './constant.js';
 
 const toThrowNewError = (condition, message) => {
   if (condition) {
@@ -18,24 +17,26 @@ const convertType = (val) => {
   return Number(val);
 };
 
-const hasStringType = (input) => {
-  const splitInputs = input.split(new RegExp(DELIMITER.map(escapeRegExp).join('|'))).map(convertType);
+const hasStringType = (input, escapedDelimiter) => {
+  const splitInputs = input.split(escapedDelimiter).map(convertType);
+
   toThrowNewError(
-    splitInputs.some((splitInput) => !Number.isInteger(splitInput)),
+    splitInputs.some((splitInput) => Number.isNaN(splitInput)),
     '구분자와 문자를 알맞게 입력해주세요. 구분자는 문자 보다 하나 적어야 합니다.',
   );
 };
 
-const isPositiveNumber = (input) => {
-  const splitInputs = input.split(new RegExp(DELIMITER.map(escapeRegExp).join('|'))).map(convertType);
+const isPositiveNumber = (input, escapedDelimiter) => {
+  const splitInputs = input.split(escapedDelimiter).map(convertType);
   toThrowNewError(
     splitInputs.some((splitInput) => splitInput <= 0),
     '1 이상만 가능합니다.',
   );
 };
 
-export const check = (input) => {
+export const check = (input, delimiter) => {
+  const escapedDelimiter = new RegExp(delimiter.map(escapeRegExp).join('|'));
   hasEmptySpace(input);
-  hasStringType(input);
-  isPositiveNumber(input);
+  hasStringType(input, escapedDelimiter);
+  isPositiveNumber(input, escapedDelimiter);
 };
