189023b4e4785dd8e541c318d30591513308ab85
dlsxjzld <sinjuk1@naver.com>
2024-11-27 15:04:04 +0900

    feat(App.js): 사용자 입력 이스케이프 처리 및 커스텀 구분자 구하기

--- src/App.js
+++ src/App.js
@@ -1,18 +1,8 @@
 import { Input, Output } from './View.js';
 
-const toThrowNewError = (condition, message) => {
-  if (condition) {
-    throw new Error(`[ERROR] ${message}\n`);
-  }
-};
+const DELIMITER = [',', ':'];
 
-const hasEmptySpace = (input) => {
-  toThrowNewError(input.includes(' '), '공백을 포함하면 안됩니다. ex)1,2');
-};
-
-const check = (input) => {
-  hasEmptySpace(input);
-};
+const escapeRegExp = (string) => string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $&은 일치한 문자열 전체를 의미
 
 class App {
   result;
@@ -21,15 +11,33 @@ class App {
     // throw new Error('[ERROR] 에러 발생 ');
     const userInput = await Input.readUserInput();
     if (userInput === '') {
-      this.result = Number(userInput);
-      Output.printResult(this.result);
+      this.checkEmptyString(userInput);
       return;
     }
+    let escapedUserInput = escapeRegExp(userInput);
 
-    check(userInput);
+    const matched = escapedUserInput.match(/\/\/(.*)\\\\n/);
+    console.log('matched', matched);
+    if (matched !== null) {
+      const [cutString, customDelimiter] = matched;
+      escapedUserInput = escapedUserInput.replace(cutString, '');
+      // console.log('escapedUserInput', escapedUserInput);
+      DELIMITER.unshift(customDelimiter);
+    }
+
+    // TODO: 커스텀 구분자 있으면 추가해야함
+    // TODO: 커스텀 구분자 추가했으면 userInput에서 커스텀 구분자 지워야함
+
     // TODO: 검증 로직 필요
 
-    Output.printResult(userInput);
+    this.input = escapedUserInput.split(new RegExp(DELIMITER.join('|'))).map(Number);
+
+    Output.printResult(escapedUserInput);
+  }
+
+  checkEmptyString(input) {
+    this.result = Number(input);
+    Output.printResult(this.result);
   }
 }
 
