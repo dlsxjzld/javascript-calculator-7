c9a858657c508a7c2e96c2c79eabdb41cb0477a1
dlsxjzld <sinjuk1@naver.com>
2024-11-27 16:15:06 +0900

    refactor(ApplicationTest.js): test.skip 삭제

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -23,7 +23,7 @@ const getLogSpy = () => {
 };
 
 describe('문자열 계산기', () => {
-  test.skip('커스텀 구분자 사용', async () => {
+  test('커스텀 구분자 사용', async () => {
     const inputs = ['//;\\n1'];
     mockQuestions(inputs);
 
@@ -38,7 +38,7 @@ describe('문자열 계산기', () => {
     });
   });
 
-  test.skip('예외 테스트', async () => {
+  test('예외 테스트', async () => {
     const inputs = ['-1,2,3'];
     mockQuestions(inputs);
 
@@ -47,30 +47,11 @@ describe('문자열 계산기', () => {
     await expect(app.run()).rejects.toThrow('[ERROR]');
   });
 
-  test.skip('예외 테스트', async () => {
-    const app = new App();
-    await expect(app.run()).rejects.toThrow('[ERROR]');
-  });
-
-  test.skip('입출력 테스트', async () => {
-    const inputs = ['1'];
-    mockQuestions(inputs);
-
-    const logSpy = getLogSpy();
-    const outputs = ['결과 : 1'];
-
-    const app = new App();
-    await app.run();
-
-    outputs.forEach((output) => {
-      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
-    });
-  });
   test.each([
     ['', '결과 : 0'],
-    // ['1,2', '결과 : 3'],
-    // ['1,2,3', '결과 : 6'],
-    // ['1,2:3', '결과 : 6'],
+    ['1,2', '결과 : 3'],
+    ['1,2,3', '결과 : 6'],
+    ['1,2:3', '결과 : 6'],
   ])('정상값 테스트', async (input, userOutput) => {
     mockQuestions([input]);
 
@@ -107,7 +88,8 @@ describe('문자열 계산기', () => {
       hasStringType('1,2a');
     }).toThrow('[ERROR]');
   });
-  test.only('0이하면 에러 발생', () => {
+
+  test('0이하면 에러 발생', () => {
     const isPositiveNumber = (input) => {
       const splitInputs = input.split(new RegExp([',', ':'].join('|'))).map(Number);
       toThrowNewError(
