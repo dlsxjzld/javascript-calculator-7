da16ab0fc76067e030eba2b99038d011aedcc76d
dlsxjzld <sinjuk1@naver.com>
2024-11-27 13:52:48 +0900

    feat(App.js): "" => 0 구현

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import { Input, Output } from './View.js';
 
+const toThrowNewError = (condition, message) => {
+  if (condition) {
+    throw new Error(`[ERROR] ${message}\n`);
+  }
+};
+
+const emptyInput = (input) => {
+  toThrowNewError(input === '');
+};
+
+const check = (input) => {
+  emptyInput(input);
+};
+
 class App {
+  result;
+
   async run() {
     // throw new Error('[ERROR] 에러 발생 ');
     const userInput = await Input.readUserInput();
+    if (userInput === '') {
+      this.result = Number(userInput);
+      Output.printResult(this.result);
+      return;
+    }
+
+    check(userInput);
+    // TODO: 검증 로직 필요
 
     Output.printResult(userInput);
   }
